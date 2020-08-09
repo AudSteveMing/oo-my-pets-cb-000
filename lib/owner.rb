3eb4c87bc6668d78f1580dfeeb556539992e629d
Steve Cordova <cordovas@bc.edu>
2020-08-09 18:58:24 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,63 @@
+require 'pry'
+
 class Owner
-  # code goes here
+
+  attr_reader :name, :species, :say_species
+
+  @@owners = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @say_species = "I am a human."
+    @@owners << self
+  end
+
+def self.all
+  @@owners
+end
+
+def self.count
+  @@owners.count
+end
+
+def self.reset_all
+  @@owners.clear
+end
+
+def cats
+  Cat.all.select do |cats|
+    cats.owner == self
+    end
+end
+
+def dogs
+  Dog.all.select {|dogs| dogs.owner == self}
+end
+
+def buy_cat(name)
+  new_owner = Cat.all.find {|cat| cat.name == name}
+  new_owner.owner = self if new_owner
+  binding.pry
+end
+
+# def buy_cat(name)
+#     Cat.new(name, self)
+#   end
+
+def buy_dog
+end
+
+def walk_dogs
+end
+
+def feed_cats
+end
+
+def sell_pets
+end
+
+def list_pets
+end
+
 end
